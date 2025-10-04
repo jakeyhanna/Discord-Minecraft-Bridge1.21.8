420e8f21cc6e14293c105c97158ceac611496365
jack <sonichanna@hotmail.com>
2025-10-05 00:02:49 +0300

    Bruh another fix, I don't know whats wrong

--- src/main/java/org/discord/discordmaybe/Discordbot.java
+++ src/main/java/org/discord/discordmaybe/Discordbot.java
@@ -1,19 +1,17 @@
 package org.discord.discordmaybe;
 
+import java.awt.Color;
+import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
-import net.minecraft.text.Text;
-import javax.security.auth.login.LoginException;
-import java.awt.*;
-
 
 public class Discordbot extends ListenerAdapter {
     private ShardManager shardManager;
@@ -28,127 +26,143 @@ public class Discordbot extends ListenerAdapter {
     }
 
     public void start() throws LoginException {
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(this.token);
         builder.setStatus(OnlineStatus.ONLINE);
-        builder.setActivity(Activity.playing("Minecraft")).enableIntents(GatewayIntent.MESSAGE_CONTENT);
-        builder.addEventListeners(this);
+        builder.setActivity(Activity.playing("Minecraft")).enableIntents(GatewayIntent.MESSAGE_CONTENT, new GatewayIntent[0]);
+        builder.addEventListeners(new Object[]{this});
         this.shardManager = builder.build();
     }
 
-    @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        // Ignore messages from bots and other channels
-        if (event.getAuthor().isBot() || event.getChannel().getIdLong() != channelId) {
-            return;
+        if (!event.getAuthor().isBot() && event.getChannel().getIdLong() == this.channelId) {
+            String var10000 = event.getAuthor().getName();
+            String message = var10000 + ": " + event.getMessage().getContentDisplay();
+            this.messageForwarder.forwardToMinecraft(message);
         }
-        // Forward Discord message to Minecraft
-        //String message = String.format("[Discord] %s: %s",
-        //        event.getAuthor().getName(),
-        //        event.getMessage().getContentDisplay());
-        String message = event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay();
-        messageForwarder.forwardToMinecraft(message);
     }
 
     public void sendToDiscord(String message) {
-        if (shardManager == null) return;
+        if (this.shardManager != null) {
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessage(message).queue();
+            }
 
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null) {
-            channel.sendMessage(message).queue();
         }
     }
 
+    public void sendEmbedMessageDiscord(String message) {
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle(message);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-    public void sendEmbedMessageDiscord(String message){
-        if (shardManager == null) return;
-        EmbedBuilder embed = new EmbedBuilder();
-                embed.setTitle(message);
-
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null){
-            channel.sendMessageEmbeds(embed.build()).queue();
         }
     }
 
     public void sendEmbedjoinDiscord(String message, String id) {
-        if (shardManager == null) return;
-        EmbedBuilder embed = new EmbedBuilder();
-                embed.setTitle("**" + message + " Joined!**");
-                embed.setDescription("Yep Yep horray");
-                embed.setThumbnail("https://api.mineatar.io/face/"+id+"?scale=12");
-                embed.setColor(Color.GREEN);
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("**" + message + " Joined!**");
+            embed.setDescription("Yep Yep horray");
+            embed.setThumbnail("https://api.mineatar.io/face/" + id + "?scale=12");
+            embed.setColor(Color.GREEN);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null){
-            channel.sendMessageEmbeds(embed.build()).queue();
         }
     }
 
     public void sendEmbedTaskDiscord(String message, String id) {
-        if (shardManager == null) return;
-        EmbedBuilder embed = new EmbedBuilder();
-        //embed.setImage("https://api.mineatar.io/face/"+id+"?scale=12");
-        //embed.setDescription(pname + "got the task" + message);
-        embed.setAuthor(message,null,"https://minotar.net/helm/"+id+"/512.png");
-        embed.setColor(Color.GREEN);
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setAuthor(message, (String)null, "https://minotar.net/helm/" + id + "/512.png");
+            embed.setColor(Color.GREEN);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null){
-            channel.sendMessageEmbeds(embed.build()).queue();
         }
     }
 
     public void sendEmbedCompletedDiscord(String message, String id) {
-        if (shardManager == null) return;
-        EmbedBuilder embed = new EmbedBuilder();
-        //embed.setImage("https://api.mineatar.io/face/"+id+"?scale=12");
-        //embed.setDescription(pname + "got the task" + message);
-        embed.setAuthor(message,null,"https://minotar.net/helm/"+id+"/512.png");
-        embed.setColor(Color.MAGENTA);
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setAuthor(message, (String)null, "https://minotar.net/helm/" + id + "/512.png");
+            embed.setColor(Color.MAGENTA);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null){
-            channel.sendMessageEmbeds(embed.build()).queue();
         }
     }
 
     public void sendEmbedDeathDiscord(String message, String id) {
-        if (shardManager == null) return;
-        EmbedBuilder embed = new EmbedBuilder();
-        //embed.setImage("https://api.mineatar.io/face/"+id+"?scale=12");
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setAuthor(message, (String)null, "https://minotar.net/helm/" + id + "/512.png");
+            embed.setColor(Color.RED);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-        //embed.setDescription(message);
-        embed.setAuthor(message,null,"https://minotar.net/helm/"+id+"/512.png");
-        embed.setColor(Color.RED);
+        }
+    }
 
+    public void sendEmbedStartserver() {
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("**:white_check_mark:SERVER HAS BEEN STARTED**");
+            embed.setColor(Color.GREEN);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null){
-            channel.sendMessageEmbeds(embed.build()).queue();
+        }
+    }
+
+    public void sendEmbedStopserver() {
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("**:x:SERVER HAS BEEN STOPPED**");
+            embed.setColor(Color.RED);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
+
         }
     }
 
     public void sendEmbedleaveDiscord(String message, String id) {
-        if (shardManager == null) return;
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.setTitle("**" + message + " left!**");
-        embed.setDescription("We hope to see you soon!");
-        embed.setThumbnail("https://api.mineatar.io/face/"+id+"?scale=12");
-        embed.setColor(Color.RED);
+        if (this.shardManager != null) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("**" + message + " left!**");
+            embed.setDescription("We hope to see you soon!");
+            embed.setThumbnail("https://api.mineatar.io/face/" + id + "?scale=12");
+            embed.setColor(Color.RED);
+            TextChannel channel = this.shardManager.getTextChannelById(this.channelId);
+            if (channel != null) {
+                channel.sendMessageEmbeds(embed.build(), new MessageEmbed[0]).queue();
+            }
 
-
-        TextChannel channel = shardManager.getTextChannelById(channelId);
-        if (channel != null){
-            channel.sendMessageEmbeds(embed.build()).queue();
         }
     }
 
     public void stop() {
-        if (shardManager != null) {
-            shardManager.shutdown();
+        if (this.shardManager != null) {
+            this.shardManager.shutdown();
         }
+
     }
 }
+ 
